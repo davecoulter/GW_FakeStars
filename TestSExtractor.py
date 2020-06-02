9ca841dc3f86c5aebc90a154e1ffb302bded23bd
Dave Coulter <davecoulter@enceladus.local>
2020-06-02 11:52:51 -0700

    more files for testing

--- TestSExtractor.py
+++ TestSExtractor.py
@@ -7,31 +7,47 @@ import re
 from runsex import runsex
 import sys
 import astropy.table as at
+from astLib import astCoords
 
 print("running SExtractor")
 
 
-sextable = runsex(file_association.image_file, segmapname=file_association.segmap_file,
-                                  zpt=fits.getval(file_association.image_file, 'ZPTMAG'))
+image_file = "s005aae0078.i.ut190425.1083_stch_1.sw.fits"
+sextable = runsex(image_file,
+                  segmapname=image_file.replace('.fits', '.check.fits'),
+                  zpt=fits.getval(image_file, 'ZPTMAG'))
 
-image_id = file_association.image_file.split('/')[-1].split('.')[0]
-glade_files = glob.glob('TileStats/*%s*txt'%image_id)
+print(sextable)
+
+field_name = image_file.split('.')[0]
+glade_files = glob.glob('*%s*txt' % field_name)
+
 for gf in glade_files:
-    #if len(glade_files):
-    #	glade_file = glade_files[0]
-    #else:
-    #	raise RuntimeError('temporarily raising error here! can\'t find GLADE file!')
-    good_ids,glade_ids,glade_bmags = np.array([]),np.array([]),np.array([])
+    good_ids, glade_ids, glade_bmags = np.array([]),np.array([]),np.array([])
 
-    try:  glade = at.Table.read(gf,format='ascii.ecsv')
-    except: continue
+    try:
+        glade = at.Table.read(gf, format='ascii.ecsv')
+    except:
+        continue
 
     for i in range(len(sextable.NUMBER)):
-        sep = astCoords.calcAngSepDeg(glade['Galaxy_RA'],glade['Galaxy_Dec'],sextable.X_WORLD[i],sextable.Y_WORLD[i])*3600.
+
+        # separation in arc sec
+        sep = astCoords.calcAngSepDeg(glade['Galaxy_RA'],
+                                      glade['Galaxy_Dec'],
+                                      sextable.X_WORLD[i],
+                                      sextable.Y_WORLD[i])*3600.
+
+        # check if it's found in the sextable...
         if not len(sep):
             break
+
+        # if within 2 arc sec
         if min(sep) < 2:
-            good_ids = np.append(good_ids,sextable.NUMBER[i])
-            glade_ids = np.append(glade_ids,glade['Galaxy_ID'][sep == np.min(sep)][0])
-            glade_bmags = np.append(glade_bmags,glade['B'][sep == np.min(sep)][0])
-if not len(glade_ids): continue
+            good_ids = np.append(good_ids, sextable.NUMBER[i])
+            glade_ids = np.append(glade_ids, glade['Galaxy_ID'][sep == np.min(sep)][0])
+            glade_bmags = np.append(glade_bmags, glade['B'][sep == np.min(sep)][0])
+
+    print(good_ids)
+    print(glade_ids)
+    print(glade_bmags)
