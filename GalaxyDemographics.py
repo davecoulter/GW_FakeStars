34b6b6e579ad81f1417bee183e7f31b97fef333d
Dave Coulter <davecoulter@enceladus.local>
2020-06-04 16:33:25 -0700

    test

--- GalaxyDemographics.py
+++ GalaxyDemographics.py
@@ -86,7 +86,7 @@ for sf in swope_files:
     mask_file = sf.replace('.fits', 'mask.fits.gz')
 
     # check if both files are on-disk
-    if os.exist(sf) and os.exists(glade_file_path) and os.exist(dcmp_file) and os._exists(mask_file):
+    if os.exists(sf) and os.exists(glade_file_path) and os.exists(dcmp_file) and os.exists(mask_file):
 
         dcmp = txtobj(dcmp_file, cmpheader=True)
         dcmp_header = fits.getheader(dcmp_file)
