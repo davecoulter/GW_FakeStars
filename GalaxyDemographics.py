07ff9c43ba580463315f31bcf21bf8cd18ca8be7
Dave Coulter <davecoulter@enceladus.local>
2020-06-05 10:51:35 -0700

    test

--- GalaxyDemographics.py
+++ GalaxyDemographics.py
@@ -78,7 +78,7 @@ num_sf = len(swope_files)
 for sf_index, sf in enumerate(swope_files):
 
     print("\n********************")
-    print("Processing %/%s..." % (sf_index, num_sf))
+    print("Processing %s/%s..." % (sf_index, num_sf))
     print("********************\n")
     t1 = time.time()
 
