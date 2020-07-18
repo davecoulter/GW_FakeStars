9ea8d8aa1acad7dd560c42cbbe452a33678c58be
Dave Coulter <davecoulter@enceladus.local>
2020-07-17 17:23:41 -0700

    debugging

--- GalaxyDemographics.py
+++ GalaxyDemographics.py
@@ -76,33 +76,38 @@ def write_good_sexcat_ids(glade_file, image_file, good_ids, glade_ids, glade_bma
         gxy = pixel_tuple[1]
         gfr = pixel_tuple[2]
 
-        print("num indices: %s" % len(ggi[0]))
+
+
+        # print("num indices: %s" % len(ggi[0]))
         s1D = Sersic1D(amplitude=1, r_eff=gfr)
         # assuming this... from Ryan (7/15/2020): "it looks like n = 2 might be a good sersic index it is both
         # between spirals and ellipticals and it avoids some issues with smaller galaxies that are only a few times
         # the size of your PSF"
         s1D.n = 2.0
-        sep = lambda x0, y0, x, y: np.sqrt((x - x0) ** 2.0 + (y - y0) ** 2.0)
-        seps = sep(gxy[0], gxy[1], np.asarray(ggi[0]), np.asarray(ggi[1]))
-        weights = s1D(seps)
 
-        print("\tSeps and Weights done.")
-
-        for x, y, s, w in zip(ggi[0], ggi[1], seps, weights):
-            weighted_pixels[sxct_id].append((x, y, s, w))
-            result_table2.add_row([sxct_id, x, y, s, w])
-
-        # arr_len = np.shape(ggi)[1]
-        # for i in range(arr_len):
-        #     x = ggi[1][i]
-        #     y = ggi[0][i]
+        # sep = lambda x0, y0, x, y: np.sqrt((x - x0) ** 2.0 + (y - y0) ** 2.0)
+        # seps = sep(gxy[0], gxy[1], np.asarray(ggi[0]), np.asarray(ggi[1]))
+        # weights = s1D(seps)
         #
-        #     sep = np.sqrt((x - gxy[0])**2.0 + (y - gxy[1])**2.0)
-        #     r = sep/gfr # fraction of a flux radius
-        #     weight = s1D(r)
+        # print("\tSeps and Weights done.")
         #
-        #     weighted_pixels[sxct_id].append((x, y, sep, weight))
-        #     result_table2.add_row([sxct_id, x, y, sep, weight])
+        # for x, y, s, w in zip(ggi[0], ggi[1], seps, weights):
+        #     weighted_pixels[sxct_id].append((x, y, s, w))
+        #     result_table2.add_row([sxct_id, x, y, s, w])
+
+
+
+        arr_len = np.shape(ggi)[1]
+        for i in range(arr_len):
+            x = ggi[1][i]
+            y = ggi[0][i]
+
+            sep = np.sqrt((x - gxy[0])**2.0 + (y - gxy[1])**2.0)
+            r = sep/gfr # fraction of a flux radius
+            weight = s1D(r)
+
+            weighted_pixels[sxct_id].append((x, y, sep, weight))
+            result_table2.add_row([sxct_id, x, y, sep, weight])
     print("Writing good pixels")
     result_table2.write(ascii_ecsv_fpath2, overwrite=True, format='ascii.ecsv')
 
